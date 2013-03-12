2726ce758e8c55b535cccc75e65e5cfdf96fffb0
Entreco <entreco@gmail.com>
2013-03-12 18:11:43 +0100

    initial commit

---
+++ Spil Games - java/src/Main.java
@@ -0,0 +1,17 @@
+
+/**
+ * {@link .Main.java}
+ *
+ * @author Entreco © Entreco - 2013
+ */
+/**
+ *
+ */
+public class Main {
+	
+	/****************************************
+	 * @param args
+	 ***************************************/
+	public static void main(String[] args) {
+	}
+}
